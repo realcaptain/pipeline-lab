9f3b76a63d9bc5a0ee916840ff93e27df2aae2f1
Mark Waite <mark.earl.waite@gmail.com>
2018-08-24 11:45:31 -0600

    Resolve checkstyle warnings for formatting and javadoc
    Rather than have a sample project that reports warnings when run with
    
    mvn checkstyle:checkstyle
    
    modified the formatting and added javadoc comments to satisfy the
    checkstyle rules.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,14 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public final class App {
+    /** main entry to hello world.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
+    /** Intentionally private constructor. */
+    private App() { }
 }
